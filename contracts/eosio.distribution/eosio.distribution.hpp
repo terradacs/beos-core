5288251081f470e22dd730752c7a6bc47f7e030e
marek <marek@syncad.com>
2018-11-09 17:14:13 +0100

    - code clean-up (close #5)

--- contracts/eosio.distribution/eosio.distribution.hpp
+++ contracts/eosio.distribution/eosio.distribution.hpp
@@ -21,13 +21,9 @@ namespace eosio {
         using ConstModifier = std::function< void( const init_data& a )>;
 
         uint64_t get_sum();
-        void review( ConstModifier&& mod );
 
         void execute( uint32_t block_nr, asset proxy_asset, uint32_t starting_block_for_any_distribution, uint32_t ending_block_for_any_distribution,
               uint32_t distribution_payment_block_interval_for_any_distribution, uint32_t nr_items, bool is_beos_mode );
-
-        void rewardall( uint32_t total_amount, asset symbol/*correct symbol of BEOS coin, for example: `0.0000 BEOS`*/, bool is_beos_mode );
-        void rewarddone( asset symbol/*correct symbol of BEOS coin, for example: `0.0000 BEOS`*/, bool is_beos_mode );
 
       public:
 
