5dd4ef570259b6ad2ce29c51759e7cdd037ba9c6
Dariusz Kedzierski <dkedzierski@syncad.com>
2018-11-14 14:49:54 +0100

    Sigint instead of sigterm in run.py. Wait.py helper script

---
+++ cd-scripts/wait.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+def usage():
+    print("Wait for process PID to close")
+    print("wait.py pid")
+
+if __name__ == "__main__":
+    import os
+    import sys
+    import time
+    argc = len(sys.argv)
+
+    if argc == 2:
+        try:
+            pid = int(sys.argv[1])
+            tries = 10000 # infinite lock protection
+            while tries > 0:
+                time.sleep(1)
+                try:
+                    os.kill(pid, 0) 
+                except ProcessLookupError: # errno.ESRCH
+                    break
+                except PermissionError: # errno.EPERM
+                    pass
+                else:
+                    pass
+                tries = tries - 1
+        except Exception as ex:
+            print("Error running script: {0}".format(ex))
+            sys.exit(1)
+    else:
+        usage()
+        sys.exit(1)
