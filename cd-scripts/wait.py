d5487050d263e93d8f59879a2024b0911cf453bc
Hayrullah Behar <>
2018-11-14 15:07:56 +0100

    update wait.py

--- cd-scripts/wait.py
+++ cd-scripts/wait.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python3
 
-<<<<<<< HEAD
-=======
-def usage():
-    print("Wait for process PID to close")
-    print("wait.py pid")
-
->>>>>>> Sigint instead of sigterm in run.py. Wait.py helper script
 if __name__ == "__main__":
     import os
     import sys
@@ -18,15 +11,7 @@ if __name__ == "__main__":
             pid = int(sys.argv[1])
             tries = 10000 # infinite lock protection
             while tries > 0:
-<<<<<<< HEAD
-<<<<<<< HEAD
                 time.sleep(0.5)
-=======
-                time.sleep(1)
->>>>>>> Sigint instead of sigterm in run.py. Wait.py helper script
-=======
-                time.sleep(0.5)
->>>>>>> Waiting time reduced to 0.5 sec per iteration
                 try:
                     os.kill(pid, 0) 
                 except ProcessLookupError: # errno.ESRCH
@@ -39,9 +24,3 @@ if __name__ == "__main__":
         except Exception as ex:
             print("Error running script: {0}".format(ex))
             sys.exit(1)
-<<<<<<< HEAD
-=======
-    else:
-        usage()
-        sys.exit(1)
->>>>>>> Sigint instead of sigterm in run.py. Wait.py helper script
