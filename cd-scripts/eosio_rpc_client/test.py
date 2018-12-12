5ef5deb7fff530f5a643fc9a6047b9b1c4b3c008
Dariusz Kedzierski <dkedzierski@syncad.com>
2018-12-12 13:55:12 +0100

    [WIP] Code simplification (removed unnecessary object) in universal RPC composer

--- cd-scripts/eosio_rpc_client/test.py
+++ cd-scripts/eosio_rpc_client/test.py
@@ -4,8 +4,7 @@
 import eosio_rpc_client
 
 if __name__ == '__main__':
-  backend = eosio_rpc_client.EosioBackend("127.0.0.1", 8888, "127.0.0.1", 8900)
-  ifce = eosio_rpc_client.EosioInterface(backend)
+  ifce = eosio_rpc_client.EosioInterface("127.0.0.1", 8888, "127.0.0.1", 8900)
 
   print("Get Info test")
   resp = ifce.chain.get_info()
