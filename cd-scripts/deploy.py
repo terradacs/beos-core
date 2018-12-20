fbc527b24c692a393c11b47e53f1a75f977798f0
Dariusz Kedzierski <dkedzierski@syncad.com>
2018-12-20 15:43:16 +0100

    Rebase fixes

--- cd-scripts/deploy.py
+++ cd-scripts/deploy.py
@@ -381,9 +381,8 @@ def initialize_beos():
         eosio_actions.set_contract("eosio.token", config.CONTRACTS_DIR + "/eosio.token", "eosio.token")
 
         eosio_actions.push_action("eosio.token", "create", '[ "eosio", "{0}"]'.format(config.CORE_TOTAL_SUPPLY), "eosio.token")
-        eosio_actions.push_action("eosio.token", "create", '[ "beos.gateway", "{0}"]'.format(config.PXBTS_TOTAL_SUPPLY), "eosio.token")
-        eosio_actions.push_action("eosio.token", "create", '[ "beos.gateway", "{0}"]'.format(config.PXBRNP_TOTAL_SUPPLY), "eosio.token")
-        eosio_actions.push_action("eosio.token", "create", '[ "beos.gateway", "{0}"]'.format(config.PXEOS_TOTAL_SUPPLY), "eosio.token")
+        for asset in config.GATEWAY_PARAMS["proxy_assets"]:
+            eosio_actions.push_action("eosio.token", "create", '[ "beos.gateway", "{0}"]'.format(asset["proxy_asset"]), "eosio.token")
 
         # registering initial producers, regproducer is in eosio.system contract so it need to be loaded first
         eosio_actions.set_contract("eosio", config.CONTRACTS_DIR + "eosio.system", "eosio")
