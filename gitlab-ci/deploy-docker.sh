683d5d4e004a3b6fbaadaca9a0f4abcb11db62d9
hbehar <hbehar@syncad.com>
2019-01-30 12:26:27 +0100

    Update deploy-docker.sh

--- gitlab-ci/deploy-docker.sh
+++ gitlab-ci/deploy-docker.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 cd ~/ci/beos-core/$CI_ENVIRONMENT_SLUG/$CI_COMMIT_REF_NAME
 
+clean_existing_images()
+{
+    docker rmi registry.gitlab.syncad.com/blocktrades/docker/beos-testnet/keosd:$BEOS_VERSION_TAG -f
+    docker rmi registry.gitlab.syncad.com/blocktrades/docker/beos-testnet/keosd:$BEOS_REVISION -f
+    docker rmi registry.gitlab.syncad.com/blocktrades/docker/beos-testnet/keosd:latest -f
+    docker rmi registry.gitlab.syncad.com/blocktrades/docker/beos-testnet/nodeos:$BEOS_VERSION_TAG -f
+    docker rmi registry.gitlab.syncad.com/blocktrades/docker/beos-testnet/nodeos:$BEOS_REVISION -f
+    docker rmi registry.gitlab.syncad.com/blocktrades/docker/beos-testnet/nodeos:latest -f
+}
+
+# Clean existing latest images and others matching the new tags 
+clean_existing_images
+
 if [ -z "$BEOS_REVISION" ]
 then
   export BEOS_REVISION=`date +'beos-testnet.%Y%m%d'`
@@ -32,10 +45,8 @@ cat beos_deploy_main.log
 
 cd ..
 
-if [ ! -d "docker-deploy" ];
-then
-  git clone git@gitlab.syncad.com:blocktrades/docker.git docker-deploy
-fi
+rm -rf docker-deploy
+git clone git@gitlab.syncad.com:blocktrades/docker.git docker-deploy
 
 cd docker-deploy
 cd beos-testnet
@@ -73,9 +84,4 @@ fi
 
 echo "Clean up images which built in this job"
 
-docker rmi registry.gitlab.syncad.com/blocktrades/docker/beos-testnet/keosd:$BEOS_VERSION_TAG -f
-docker rmi registry.gitlab.syncad.com/blocktrades/docker/beos-testnet/keosd:$BEOS_REVISION -f
-docker rmi registry.gitlab.syncad.com/blocktrades/docker/beos-testnet/keosd:latest -f
-docker rmi registry.gitlab.syncad.com/blocktrades/docker/beos-testnet/nodeos:$BEOS_VERSION_TAG -f
-docker rmi registry.gitlab.syncad.com/blocktrades/docker/beos-testnet/nodeos:$BEOS_REVISION -f
-docker rmi registry.gitlab.syncad.com/blocktrades/docker/beos-testnet/nodeos:latest -f
+clean_existing_images
