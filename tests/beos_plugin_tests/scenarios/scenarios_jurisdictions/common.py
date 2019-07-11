cd738b2f5d64fc5f438581c741e546cc65f76435
Dariusz Kedzierski <dkedzierski@syncad.com>
2019-07-11 16:46:23 +0200

    Fix for new return structure in case of transaction being deferred due to the jurisdiction

--- tests/beos_plugin_tests/scenarios/scenarios_jurisdictions/common.py
+++ tests/beos_plugin_tests/scenarios/scenarios_jurisdictions/common.py
@@ -1,10 +1,5 @@
-def get_transaction_id_from_result(code, result):
-  import re
-  ansi_escape = re.compile(r'\x1B\[[0-?]*[ -/]*[@-~]')
-  result_str = ansi_escape.sub('', result)
+import json
 
-  result_str = result_str.strip().split("\n")
-  if code == 0:
-    return str(result_str[0].strip().split(" ")[2].strip())
-  else:
-    return str(result_str[-1].strip().split(" ")[-1].strip())
+def get_transaction_id_from_result(code, result):
+  data = json.loads(result)
+  return data["trx_id"]
