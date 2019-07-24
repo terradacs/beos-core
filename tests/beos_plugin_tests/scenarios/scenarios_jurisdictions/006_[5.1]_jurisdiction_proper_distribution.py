cdbf44993b5f12621889ed16474ddf23ff0f64dd
Wieslaw Kedzierski <wkedzierski@syncad.com>
2019-07-24 11:40:59 +0200

    [WKE] Add scenario checking proper transaction with jurisdiction to block assigment.

---
+++ tests/beos_plugin_tests/scenarios/scenarios_jurisdictions/006_[5.1]_jurisdiction_proper_distribution.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import time
+import json
+import datetime 
+import requests
+
+currentdir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(os.path.dirname(currentdir)))
+from beos_test_utils.beos_utils_pack import init, init_cluster, ActionResult, ResourceResult, VotersResult
+from common import get_transaction_id_from_result
+
+if __name__ == "__main__":
+	try:
+		number_of_pnodes  = 3
+		producer_per_node = 1
+		cluster, summary, args, log = init_cluster(__file__, number_of_pnodes, producer_per_node)
+		cluster.run_all()
+
+		prods=[]
+		for prod, data in cluster.producers.items(): 
+			prods.append(prod)
+
+		call = ["push", "action", "eosio", "addjurisdict", '[ "eosio", "0", "POLAND", "EAST EUROPE" ]', "-p", "eosio"]
+		code, result = cluster.bios.make_cleos_call(call)
+		log.info(f"{result}")
+
+		call = ["push", "action", "eosio", "addjurisdict", '[ "eosio", "1", "GERMANY", "EAST EUROPE" ]', "-p", "eosio"]
+		code, result = cluster.bios.make_cleos_call(call)
+		log.info(f"{result}")
+
+		call = ["push", "action", "eosio", "addjurisdict", '[ "eosio", "2", "RUSSIA", "EAST EUROPE" ]', "-p", "eosio"]
+		code, result = cluster.bios.make_cleos_call(call)
+		log.info(f"{result}")
+
+		prods_0_jurisdiction = 0
+		prods_1_jurisdiction = 1
+		prods_2_jurisdiction = 2
+
+		call = ["push", "action", "eosio", "updateprod", '{{"data":{{"producer":"{0}", "jurisdictions":[{1}]}} }}'.format(prods[0], prods_0_jurisdiction), "-p", f"{prods[0]}"]
+		code, result = cluster.bios.make_cleos_call(call)
+		log.info(f"{result}")
+
+		call = ["push", "action", "eosio", "updateprod", '{{"data":{{"producer":"{0}", "jurisdictions":[{1}]}} }}'.format(prods[1], prods_1_jurisdiction), "-p", f"{prods[1]}"]
+		code, result = cluster.bios.make_cleos_call(call)
+		log.info(f"{result}")
+
+		call = ["push", "action", "eosio", "updateprod", '{{"data":{{"producer":"{0}", "jurisdictions":[{1}]}} }}'.format(prods[2], prods_2_jurisdiction), "-p", f"{prods[2]}"]
+		code, result = cluster.bios.make_cleos_call(call)
+		log.info(f"{result}")
+
+
+		call =[ "push", "action", "--jurisdictions", "[{0}]".format(prods_1_jurisdiction), "beos.gateway", "issue", "[ \"{0}\", \"111.0000 BTS\", \"hello\" ]".format(prods[0]), "-p", "beos.gateway"]
+		code, result = cluster.bios.make_cleos_call(call)
+		log.info(f"{result}")
+
+		trx_id = get_transaction_id_from_result(code, result)
+		log.info("Waiting for transaction id: {} in block".format(trx_id))
+		block = cluster.bios.wait_for_transaction_in_block(trx_id)
+
+		call = ["get", "block", "{0}".format(block)]
+		code, result = cluster.bios.make_cleos_call(call)
+		log.info(result)
+		result = json.loads(result)
+		summary.equal(True, result["producer"] == "{0}".format(prods[1]), "This transaction should be signed by producer {0}".format(prods[1]))
+
+		call = ["get", "producer_jurisdiction_for_block", "{0}".format(prods[1]), "{0}".format(block)]
+		code, result = cluster.bios.make_cleos_call(call)
+		log.info(result)
+		result = json.loads(result)
+		summary.equal(True, prods_1_jurisdiction in result["producer_jurisdiction_for_block"][0]["new_jurisdictions"], "Jurisdiction `{0}` should be assigned to {1}".format(prods_1_jurisdiction, prods[1]))
+
+		call =[ "push", "action", "--jurisdictions", "[{0}]".format(prods_0_jurisdiction), "beos.gateway", "issue", "[ \"{0}\", \"122.0000 BTS\", \"hello\" ]".format(prods[1]), "-p", "beos.gateway"]
+		code, result = cluster.bios.make_cleos_call(call)
+		log.info(f"{result}")
+
+		trx_id = get_transaction_id_from_result(code, result)
+		log.info("Waiting for transaction id: {} in block".format(trx_id))
+		block = cluster.bios.wait_for_transaction_in_block(trx_id)
+
+		call = ["get", "block", "{0}".format(block)]
+		code, result = cluster.bios.make_cleos_call(call)
+		log.info(result)
+		result = json.loads(result)
+
+		summary.equal(True, result["producer"] == "{0}".format(prods[0]), "This transaction should be signed by producer {0}".format(prods[0]))
+
+		call = ["get", "producer_jurisdiction_for_block", "{0}".format(prods[0]), "{0}".format(block)]
+		code, result = cluster.bios.make_cleos_call(call)
+		log.info(result)
+		result = json.loads(result)
+		summary.equal(True, prods_0_jurisdiction in result["producer_jurisdiction_for_block"][0]["new_jurisdictions"], "Jurisdiction `{0}` should be assigned to {1}".format(prods_0_jurisdiction,prods[0]))
+
+	except Exception as _ex:
+		log.exception(_ex)
+		summary.equal(True, False, "There should be no exceptions.")
+	finally:
+		status = summary.summarize()
+		cluster.stop_all()
+		exit(status)
